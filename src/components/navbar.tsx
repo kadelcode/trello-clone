8263cdd45b91b519848b63fb123f8352ee7193dc
kadelcode <kadelcode@gmail.com>
2025-03-01 13:34:11 +0100

    Comment code for testing out linting

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,4 @@
-"use client"
+/*"use client"
 
 import Link from "next/link"
 import { useRouter } from "next/navigation"
@@ -62,4 +62,4 @@ export default function Navbar({ user }: {user: any}) {
             </div>
         </nav>
     )
-}
+}*/
